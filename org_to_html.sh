4212f528a42520d8c296f940a5a8526efb08550d
utsavdarlami <utsav.darlami@docsumo.com>
2024-10-09 18:04:45 +0545

    update: org to html hook | test 3

--- org_to_html.sh
+++ org_to_html.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
-echo "org to html"
+# Get the list of modified files in the commit
+modified_files=$(git diff --cached --name-only)
 
-emacsclient -e '(progn (find-file "log.org") (org-html-export-to-html) (bury-buffer))'
+# Check if log.org is modified and index.html is NOT modified
+if echo "$modified_files" | grep -q "log.org" && ! echo "$modified_files" | grep -q "index.html"; then
+    echo "Running org to HTML export for log.org..."
+
+    # Run the org to HTML conversion command
+    emacsclient -e '(progn (find-file "log.org") (org-html-export-to-html) (bury-buffer))'
+else
+    echo "Skipping org to HTML export. Either log.org is not modified or index.html is modified."
+fi
